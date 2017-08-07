ea1289aa06c288c14c2ec2c89810c546e56f21fb
Heinz N. Gies <heinz@licenser.net>
2017-08-08 01:52:22 +0200

    fuck travis

--- ci.sh
+++ ci.sh
@@ -4,5 +4,6 @@ curl -Ol http://www.quviq.com/wp-content/uploads/2015/09/eqcmini-2.01.0.zip
 unzip eqcmini-2.01.0.zip
 cd ..
 ERL_LIBS=./libs
+find / -name empd
 empd &
 ./rebar3 as eqc eqc --sname eqc
